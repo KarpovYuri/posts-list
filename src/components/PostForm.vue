2c6e202852baf2e6da1ea182de38bf9970929813
Karpov Yuri <yurick@bk.ru>
2023-05-06 23:36:13 +0300

    feat: Удаление постов

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,19 +1,9 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <input
-      v-model="post.title"
-      class="input"
-      type="text"
-      placeholder="Название"
-    />
-    <input
-      v-model="post.body"
-      class="input"
-      type="text"
-      placeholder="Описание"
-    />
-    <button class="btn" @click="createPost">Создать</button>
+    <MyInput v-model="post.title" placeholder="Название" />
+    <MyInput v-model="post.body" placeholder="Описание" />
+    <MyButton class="btn_create" @click="createPost">Создать</MyButton>
   </form>
 </template>
 
@@ -45,20 +35,9 @@ form {
   display: flex;
   flex-direction: column;
 }
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
 
-.btn {
+.btn_create {
   margin-top: 15px;
   align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
-  cursor: pointer;
 }
 </style>
